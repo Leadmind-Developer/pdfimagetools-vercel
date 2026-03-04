cb810062d8f21a4ab457dcfe9fe0a8705140d321
Fortemedia <124510838+Fortemedia@users.noreply.github.com>
2026-03-04 17:42:36 +0100

    Update layout.js

--- src/app/blog/layout.js
+++ src/app/blog/layout.js
@@ -1,18 +1,48 @@
+"use client";
+
 import BlogHeader from "../../components/BlogHeader";
 import BlogFooter from "../../components/BlogFooter";
+import BidvertiserNativeAd from "../../components/ads/BidvertiserNativeAd";
+import React from "react";
 
 export default function BlogLayout({ children }) {
+
+  const contentArray = React.Children.toArray(children);
+
   return (
     <>
-      {/* Minimal Blog Header */}
       <BlogHeader />
 
-      {/* Blog Content */}
       <main className="max-w-4xl mx-auto px-4 py-12 leading-relaxed">
-        {children}
+
+        {contentArray.map((child, index) => (
+          <React.Fragment key={index}>
+
+            {child}
+
+            {/* After 2nd block */}
+            {index === 1 && (
+              <BidvertiserNativeAd
+                rows={1}
+                cols={3}
+                imagewidth={180}
+              />
+            )}
+
+            {/* Mid article */}
+            {index === Math.floor(contentArray.length / 2) && (
+              <BidvertiserNativeAd
+                rows={1}
+                cols={3}
+                imagewidth={180}
+              />
+            )}
+
+          </React.Fragment>
+        ))}
+
       </main>
 
-      {/* Blog Footer */}
       <BlogFooter />
     </>
   );
