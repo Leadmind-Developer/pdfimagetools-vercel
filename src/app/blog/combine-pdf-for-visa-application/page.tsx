cc4844e9c033ff6094c5360ef231c4ae3f8c2786
Fortemedia <124510838+Fortemedia@users.noreply.github.com>
2026-03-05 16:02:03 +0100

    Update page.tsx

--- src/app/blog/combine-pdf-for-visa-application/page.tsx
+++ src/app/blog/combine-pdf-for-visa-application/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Link from "next/link";
-import BlogFooter from "../../../components/BlogFooter";
 import BlogAd from "@/components/ads/BlogAd";
 
 export const metadata = {
